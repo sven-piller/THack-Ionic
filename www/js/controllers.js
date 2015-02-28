ffd2c4198e7a90ad23f9ade8c4edbd28fa9b1ad7
Sven Piller <sven.piller@gmail.com>
2015-02-28 18:18:33 +0100

    temp Design steht

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,60 +1,97 @@
 angular.module('starter.controllers', [])
 
-.controller('Q1Ctrl', ['$scope', '$http', function($scope, $http) {
-	$scope.typeList = [{
-		text: "Couple",
-		img: "img/beach.jpg",
-		checked: false,
-		id: 1
-	}, {
-		text: "Family",
-		img: "img/family.jpg",
-		checked: false,
-		id: 2
-	}, {
-		text: "Single",
-		img: "img/camping.jpg",
-		checked: false,
-		id: 3
-	}, {
-		text: "Friends",
-		img: "img/city.jpg",
-		checked: false,
-		id: 4
-	}];
-}])
+.controller('Q1Ctrl', ['$scope', '$http', '$rootScope', '$stateParams',
+	function($scope, $http, $rootScope, $stateParams) {
+		$scope.responseData = null;
+		$rootScope.request = {};
+		$scope.typeList = [{
+			text: "Couple",
+			img: "img/beach.jpg",
+			checked: false,
+			id: 1
+		}, {
+			text: "Family",
+			img: "img/family.jpg",
+			checked: false,
+			id: 2
+		}, {
+			text: "Single",
+			img: "img/camping.jpg",
+			checked: false,
+			id: 3
+		}, {
+			text: "Friends",
+			img: "img/city.jpg",
+			checked: false,
+			id: 4
+		}];
+	}
+])
 
-.controller('Q2Ctrl', ['$scope', '$http', function($scope, $http) {
-	$scope.destinationList = [{
-		text: "Beach",
-		img: "img/beach.jpg",
-		checked: false,
-		id: 1
-	}, {
-		text: "Countryside",
-		img: "img/countryside.jpg",
-		checked: false,
-		id: 2
-	}, {
-		text: "Camping",
-		img: "img/camping.jpg",
-		checked: false,
-		id: 3
-	}, {
-		text: "City",
-		img: "img/city.jpg",
-		checked: false,
-		id: 4
-	}];
-}])
+.controller('Q2Ctrl', ['$scope', '$http', '$rootScope', '$stateParams',
+	function($scope, $http, $rootScope, $stateParams) {
+		console.log($stateParams);
+		$rootScope.request.type = $stateParams.type;
+		$scope.destinationList = [{
+			text: "Beach",
+			img: "img/beach.jpg",
+			checked: false,
+			id: 1
+		}, {
+			text: "Countryside",
+			img: "img/countryside.jpg",
+			checked: false,
+			id: 2
+		}, {
+			text: "Camping",
+			img: "img/camping.jpg",
+			checked: false,
+			id: 3
+		}, {
+			text: "City",
+			img: "img/city.jpg",
+			checked: false,
+			id: 4
+		}];
+	}
+])
 
-.controller('Q3Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('Q3Ctrl', ['$scope', '$http', '$rootScope', '$stateParams',
+	function($scope, $http, $rootScope, $stateParams) {
+		console.log($stateParams);
+		$rootScope.request.destination = $stateParams.destination;
+	}
+])
 
-}])
+.controller('Q4Ctrl', ['$scope', '$http', '$rootScope', '$stateParams',
+	function($scope, $http, $rootScope, $stateParams) {
+		console.log($stateParams);
+		$rootScope.request.date = $stateParams.date;
+	}
+])
 
-.controller('Q4Ctrl', ['$scope', '$http', function($scope, $http) {
+/*.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
 
-	$scope.sendRequest = function() {
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+*/
+.controller('ResultCtrl', function($scope, $http, $rootScope, $stateParams,
+	$ionicLoading) {
+	console.log($stateParams);
+	$ionicLoading.show({
+			template: 'loading'
+		})
+		//$rootScope.request.money = $stateParams.money;
+	$scope.responseData = null;
+
+	//console.log(JSON.stringify($rootScope.request));
+	//var requestData = $rootScope.request;
+
+	/*
+	sendRequest = function() {
 		var request = $http({
 			method: 'POST',
 			url: '',
@@ -68,24 +105,16 @@ angular.module('starter.controllers', [])
 						enidURIComponent(obj[p]));
 				return str.join("&");
 			},
-			data: {
-				name: $scope.name,
-				flight: $scope.flight,
-				name_pickuper: $scope.name_pickuper,
-				email_pickuper: $scope.email_pickuper,
-				message: $scope.message,
-				uid: $scope.unique_request
-			}
+			data: requestData
 		});
 		request.success(
 			function(data, status, headers, config) {
-
 				//$scope.cfdump = data;
 				console.log("success");
 				//console.log($scope.cfdump);
 				console.log(JSON.stringify(data));
-				$scope.response_data = data;
-				$scope.showCard = true;
+				$scope.responseData = data;
+				$scope.showResults = true;
 			}
 		);
 		request.error(
@@ -98,22 +127,18 @@ angular.module('starter.controllers', [])
 				//console.log("config -> " + JSON.stringify(config));
 			}
 		);
-	};
-}])
+	};*/
 
-/*.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+	setTimeout(function sendRequest() {
+		$scope.responseData = [{
+			'id': '332222',
+			'flight': 'LH400'
+		}, {
+			'id': '221',
+			'flight': 'LH800'
+		}];
+		$ionicLoading.hide();
+		console.log(JSON.stringify($scope.responseData));
+	}, 1000);
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-*/
-.controller('RequestCtrl', function($scope) {
-	/*$scope.tasks = [
-	  { title: 'Collect coins' },
-	  { title: 'Eat mushrooms' },
-	  { title: 'Get high enough to grab the flag' },
-	  { title: 'Find the Princess' }
-	];*/
 });
