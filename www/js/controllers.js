a1026149665c09f8b92e2a2db84279852ad1bbd7
Sven Piller <sven.piller@gmail.com>
2015-03-01 12:25:15 +0100

    final für die abgabe

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,7 +6,7 @@ angular.module('starter.controllers', [])
 		$rootScope.request = {};
 		$scope.typeList = [{
 			text: "JUST ME",
-			img: "img/beach.jpg",
+			img: "img/single.png",
 			checked: false,
 			id: 1,
 			color: '#006B88'
@@ -18,13 +18,13 @@ angular.module('starter.controllers', [])
 			color: '#D4382B'
 		}, {
 			text: "FRIENDS",
-			img: "img/camping.jpg",
+			img: "img/friends.png",
 			checked: false,
 			id: 3,
 			color: '#7B8EC6'
 		}, {
 			text: "COUPLES",
-			img: "img/city.jpg",
+			img: "img/couple.png",
 			checked: false,
 			id: 4,
 			color: '#A39685'
@@ -44,19 +44,19 @@ angular.module('starter.controllers', [])
 			color: '#006B88'
 		}, {
 			text: "URBAN",
-			img: "img/countryside.jpg",
+			img: "img/city.jpg",
 			checked: false,
 			id: 2,
 			color: '#D4382B'
 		}, {
 			text: "COUNTRY",
-			img: "img/camping.jpg",
+			img: "img/countryside.jpg",
 			checked: false,
 			id: 3,
 			color: '#7B8EC6'
 		}, {
 			text: "SKI",
-			img: "img/city.jpg",
+			img: "img/ski.png",
 			checked: false,
 			id: 4,
 			color: '#A39685'
@@ -69,26 +69,26 @@ angular.module('starter.controllers', [])
 		console.log($stateParams);
 		$rootScope.request.destination = $stateParams.destination;
 		$scope.monthList = [{
-			text: "1-3",
-			img: "img/city.jpg",
+			text: "Winter",
+			img: "img/winter.png",
 			checked: false,
 			id: 1,
 			color: '#006B88'
 		}, {
-			text: "4-6",
-			img: "img/city.jpg",
+			text: "Spring",
+			img: "img/spring.png",
 			checked: false,
 			id: 2,
 			color: '#D4382B'
 		}, {
-			text: "7-9",
-			img: "img/beach.jpg",
+			text: "Summer",
+			img: "img/summer.png",
 			checked: false,
 			id: 3,
 			color: '#7B8EC6'
 		}, {
-			text: "10-12",
-			img: "img/beach.jpg",
+			text: "Autumn",
+			img: "img/autumn.png",
 			checked: false,
 			id: 4,
 			color: '#A39685'
@@ -102,25 +102,25 @@ angular.module('starter.controllers', [])
 		$rootScope.request.date = $stateParams.date;
 		$scope.budgetList = [{
 			text: "300",
-			img: "img/beach.jpg",
+			img: "img/b1.png",
 			checked: false,
 			id: 1,
 			color: '#006B88'
 		}, {
 			text: "500",
-			img: "img/beach.jpg",
+			img: "img/b2.png",
 			checked: false,
 			id: 2,
 			color: '#D4382B'
 		}, {
 			text: "750",
-			img: "img/beach.jpg",
+			img: "img/b3.png",
 			checked: false,
 			id: 3,
 			color: '#7B8EC6'
 		}, {
 			text: "1000",
-			img: "img/beach.jpg",
+			img: "img/b4.png",
 			checked: false,
 			id: 4,
 			color: '#A39685'
@@ -148,61 +148,89 @@ angular.module('starter.controllers', [])
 		"persons": "2",
 		"type": "FastSearch"
 	};
+	/*
+	var requestData = {
+		'type': "family",
+		'destination': "beach",
+		'month': '8',
+		'budget': '2'
 
-	var reQuestData = {
-			'type': "family",
-			'destination': "beach",
-			'month': '8',
-			'budget': '2'
+	};*/
+	// https://nameless-bastion-4712.herokuapp.com/flights.json?Origin=MUC&Destination=LHR&DepartureFrom=2015-06-18&LengthOfStay=4
+	// &DepartureTo=2015-07-18&persons=2&type=FastSearch
 
-		}
-		// https://nameless-bastion-4712.herokuapp.com/flights.json?Origin=MUC&Destination=LHR&DepartureFrom=2015-06-18&LengthOfStay=4
-		// &DepartureTo=2015-07-18&persons=2&type=FastSearch
-		/*
-		  sendRequest = function() {
-		    var request = $http({
-		      method: 'POST',
-		      url: 'http://travless.com/hackton',
-		      headers: {
-		        'Content-Type': 'application/json'
-		      },
-		      data: requestData
-		    });
-		    request.success(
-		      function(data, status, headers, config) {
-		        //$scope.cfdump = data;
-		        console.log("success");
-		        //console.log($scope.cfdump);
-		        console.log(JSON.stringify(data));
-		        $scope.responseData = data;
+	// TODO FIXME: CORS-Issues, therefor an mockup of the answer result
 
-		        $ionicLoading.hide();
-		      }
-		    );
-		    request.error(
-		      function(data, status, headers, config) {
-		        console.log("error");
-		        //console.log($scope.cfdump);
-		        console.log("data -> " + JSON.stringify(data));
-		        console.log("status -> " + status);
-		        //console.log("headers -> " + headers);
-		        //console.log("config -> " + JSON.stringify(config));
-		      }
-		    );
-		  };*/
+	/*	sendRequest = function() {
+			var request = $http({
+				method: 'POST',
+				url: 'http://travless.com/hackton/index.php',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				data: requestData
+			});
+			request.success(
+				function(data, status, headers, config) {
+					//$scope.cfdump = data;
+					console.log("success");
+					//console.log($scope.cfdump);
+					console.log(JSON.stringify(data));
+					$scope.responseData = data;
+
+					$ionicLoading.hide();
+				}
+			);
+			request.error(
+				function(data, status, headers, config) {
+					console.log("error");
+					//console.log($scope.cfdump);
+					console.log("data -> " + JSON.stringify(data));
+					console.log("status -> " + status);
+					// console.log("headers -> " + headers);
+					//console.log("config -> " + JSON.stringify(config));
+				}
+			);
+		};
+		sendRequest();*/
 
 	setTimeout(function sendRequest() {
 		$scope.responseData = [{
 			'id': '332222',
-			'destination': 'MUC',
+			'destination': 'BUD',
+			'destination_name': 'Budapest',
 			'color': '#006B88',
-			'price': '678,55'
-
+			'price': '678.55',
+			'hotel': 'Hilton',
+			'class': '5',
+			'date': '2015-07-12'
 		}, {
 			'id': '221',
-			'destination': 'LAX',
-			'color': '#006B88',
-			'price': '123,55'
+			'destination': 'DBX',
+			'destination_name': 'Dubai',
+			'color': '#D4382B',
+			'price': '799.99',
+			'hotel': 'Four Seasons',
+			'class': '5',
+			'date': '2015-07-06'
+		}, {
+			'id': '123',
+			'destination': 'HAM',
+			'destination_name': 'Hamburg',
+			'color': '#7B8EC6',
+			'price': '564.55',
+			'hotel': 'Zum Goldenen Hirsch',
+			'class': '5',
+			'date': '2015-07-18'
+		}, {
+			'id': '124',
+			'destination': 'JFK',
+			'destination_name': 'New York',
+			'color': '#A39685',
+			'price': '723.55',
+			'hotel': 'Hilton',
+			'class': '5',
+			'date': '2015-07-23'
 		}];
 		$ionicLoading.hide();
 		console.log(JSON.stringify($scope.responseData));
@@ -214,11 +242,12 @@ angular.module('starter.controllers', [])
 	function($scope, $http, $rootScope, $stateParams) {
 		console.log($stateParams);
 		$scope.detail = {
-			'id': $stateParams.id,
-			'flight': 'LH400',
-			'destination': 'MUC',
-			'color': '#006B88',
-			'price': '678,55'
+			'destination': 'DBX',
+			'destination_name': 'Dubai',
+			'price': ' € 799.99',
+			'hotel': 'Four Seasons',
+			'class': '5 Stars',
+			'date': '2015-07-06'
 		};
 	}
 ]);
